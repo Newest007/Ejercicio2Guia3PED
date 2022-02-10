2988e49c3f0a4cd45cac0fa75955ac5b2a00df5a
Arian <adilsonarian@outlook.com>
2022-02-10 13:29:34 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿
+namespace Ejercicio_2_Guía_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnHacerFila1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.timerCajero1Mover = new System.Windows.Forms.Timer(this.components);
+            this.timerCajero1Eliminar = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btnQuitarCliente = new System.Windows.Forms.Button();
+            this.btnHacerFila2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timerCajero2Mover = new System.Windows.Forms.Timer(this.components);
+            this.timerCajero2Eliminar = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(164, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido al Banco de Halfonso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(396, 121);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 26);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Cajero 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(541, 121);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 26);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Cajero 4";
+            // 
+            // btnHacerFila1
+            // 
+            this.btnHacerFila1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(55)))), ((int)(((byte)(83)))));
+            this.btnHacerFila1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHacerFila1.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHacerFila1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHacerFila1.Location = new System.Drawing.Point(39, 116);
+            this.btnHacerFila1.Name = "btnHacerFila1";
+            this.btnHacerFila1.Size = new System.Drawing.Size(105, 37);
+            this.btnHacerFila1.TabIndex = 6;
+            this.btnHacerFila1.Text = "Cajero 1";
+            this.btnHacerFila1.UseVisualStyleBackColor = false;
+            this.btnHacerFila1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(68, 395);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(68, 407);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(68, 417);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // timerCajero1Mover
+            // 
+            this.timerCajero1Mover.Interval = 10;
+            this.timerCajero1Mover.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timerCajero1Eliminar
+            // 
+            this.timerCajero1Eliminar.Interval = 10;
+            this.timerCajero1Eliminar.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(68, 435);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(68, 425);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btnQuitarCliente
+            // 
+            this.btnQuitarCliente.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(15)))), ((int)(((byte)(29)))));
+            this.btnQuitarCliente.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnQuitarCliente.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuitarCliente.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnQuitarCliente.Location = new System.Drawing.Point(28, 68);
+            this.btnQuitarCliente.Name = "btnQuitarCliente";
+            this.btnQuitarCliente.Size = new System.Drawing.Size(132, 37);
+            this.btnQuitarCliente.TabIndex = 13;
+            this.btnQuitarCliente.Text = "Despachar";
+            this.btnQuitarCliente.UseVisualStyleBackColor = false;
+            this.btnQuitarCliente.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnHacerFila2
+            // 
+            this.btnHacerFila2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(55)))), ((int)(((byte)(83)))));
+            this.btnHacerFila2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnHacerFila2.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHacerFila2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHacerFila2.Location = new System.Drawing.Point(214, 116);
+            this.btnHacerFila2.Name = "btnHacerFila2";
+            this.btnHacerFila2.Size = new System.Drawing.Size(105, 37);
+            this.btnHacerFila2.TabIndex = 15;
+            this.btnHacerFila2.Text = "Cajero 2";
+            this.btnHacerFila2.UseVisualStyleBackColor = false;
+            this.btnHacerFila2.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(33)))), ((int)(((byte)(122)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(656, 62);
+            this.panel1.TabIndex = 16;
+            // 
+            // timerCajero2Mover
+            // 
+            this.timerCajero2Mover.Interval = 10;
+            this.timerCajero2Mover.Tick += new System.EventHandler(this.timerCajero2Mover_Tick);
+            // 
+            // timerCajero2Eliminar
+            // 
+            this.timerCajero2Eliminar.Interval = 10;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox10.Image")));
+            this.pictureBox10.Location = new System.Drawing.Point(250, 435);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 20;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox9.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox9.Image")));
+            this.pictureBox9.Location = new System.Drawing.Point(250, 425);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 21;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox8.Image")));
+            this.pictureBox8.Location = new System.Drawing.Point(250, 417);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 19;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(250, 407);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 18;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox6.InitialImage")));
+            this.pictureBox6.Location = new System.Drawing.Point(250, 395);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(30, 41);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 17;
+            this.pictureBox6.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 453);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnHacerFila2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.btnQuitarCliente);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnHacerFila1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Font = new System.Drawing.Font("Segoe Print", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnHacerFila1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timerCajero1Mover;
+        private System.Windows.Forms.Timer timerCajero1Eliminar;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btnQuitarCliente;
+        private System.Windows.Forms.Button btnHacerFila2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timerCajero2Mover;
+        private System.Windows.Forms.Timer timerCajero2Eliminar;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_2_Guía_3
+{
+    public partial class Form1 : Form
+    {
+
+        int totalCajero1 = 0; //Objeto usado para calcular el numero de personas en el cajero 1
+        int totalCajero2 = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(totalCajero1 == 5)
+            {
+                MessageBox.Show("La cola esta llena", "Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                totalCajero1++;
+                timerCajero1Mover.Enabled = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            btnHacerFila1.Enabled = false;
+
+            if(totalCajero1 == 1)
+            {
+                pictureBox1.Top = pictureBox1.Top - 5;
+                if (pictureBox1.Top <= 160) timerCajero1Mover.Enabled = false;
+            }
+
+            if(totalCajero1 == 2)
+            {
+                pictureBox2.Top = pictureBox2.Top - 5;
+                if (pictureBox2.Top <= 205) timerCajero1Mover.Enabled = false;
+            }
+
+            if(totalCajero1 == 3)
+            {
+                pictureBox3.Top = pictureBox3.Top - 5;
+                if (pictureBox3.Top <= 250) timerCajero1Mover.Enabled = false;
+            }
+
+            if (totalCajero1 == 4)
+            {
+                pictureBox4.Top = pictureBox4.Top - 5;
+                if (pictureBox4.Top <= 295) timerCajero1Mover.Enabled = false;
+            }
+
+            if(totalCajero1 == 5)
+            {
+                pictureBox5.Top = pictureBox5.Top - 5;
+                if (pictureBox5.Top <= 340) timerCajero1Mover.Enabled = false;
+            }
+
+            if (timerCajero1Mover.Enabled == false) btnHacerFila1.Enabled = true;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            btnQuitarCliente.Enabled = false;
+            pictureBox1.Left = pictureBox1.Left + 5;
+
+            if(pictureBox1.Left >= 100)
+            {
+                pictureBox1.Left = 68;
+                if (totalCajero1 == 4) pictureBox5.Location = new Point(68, 395);
+                if (totalCajero1 == 3) pictureBox4.Location = new Point(68, 395);
+                if (totalCajero1 == 2) pictureBox3.Location = new Point(68, 395);
+                if (totalCajero1 == 1) pictureBox2.Location = new Point(68, 395);
+                if (totalCajero1 == 0) pictureBox1.Location = new Point(68, 395);
+                timerCajero1Eliminar.Enabled = false;
+            }
+            if (timerCajero1Eliminar.Enabled == false) btnQuitarCliente.Enabled = true;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (totalCajero1 == 0) { }
+            else
+            {
+                timerCajero1Eliminar.Enabled = true;
+                totalCajero1--;
+            }
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            if(totalCajero2 == 5)
+            {
+                MessageBox.Show("La cola esta llena", "Advertencia!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                totalCajero2++;
+                timerCajero2Mover.Enabled = true;
+            }
+        }
+
+        private void timerCajero2Mover_Tick(object sender, EventArgs e)
+        {
+            btnHacerFila2.Enabled = false;
+
+            if (totalCajero2 == 1)
+            {
+                pictureBox6.Top = pictureBox6.Top - 5;
+                if (pictureBox6.Top <= 160) timerCajero2Mover.Enabled = false;
+            }
+
+            if (totalCajero2 == 2)
+            {
+                pictureBox7.Top = pictureBox7.Top - 5;
+                if (pictureBox7.Top <= 205) timerCajero2Mover.Enabled = false;
+            }
+
+            if (totalCajero2 == 3)
+            {
+                pictureBox8.Top = pictureBox8.Top - 5;
+                if (pictureBox8.Top <= 250) timerCajero2Mover.Enabled = false;
+            }
+
+            if (totalCajero2 == 4)
+            {
+                pictureBox9.Top = pictureBox9.Top - 5;
+                if (pictureBox9.Top <= 295) timerCajero2Mover.Enabled = false;
+            }
+
+            if (totalCajero2 == 5)
+            {
+                pictureBox10.Top = pictureBox10.Top - 5;
+                if (pictureBox10.Top <= 340) timerCajero2Mover.Enabled = false;
+            }
+
+            if (timerCajero2Mover.Enabled == false) btnHacerFila2.Enabled = true;
+
+        }
+    }
+}
